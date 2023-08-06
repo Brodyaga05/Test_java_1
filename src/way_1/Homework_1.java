1ecd4830bda2a71f1b35bfcf3eef282921e1957f
Brodyaga05 <ulmart035@gmail.com>
2023-08-06 19:15:23 +0300

    commit 4

--- src/way_1/Homework_1.java
+++ src/way_1/Homework_1.java
@@ -1,4 +1,12 @@
 package way_1;
 
 public class Homework_1 {
+    public static void main(String[] args) {
+        task();
+    }
+        private static void task(){
+            System.out.println("Решение");
+        }
+
+
 }
